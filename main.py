f5bb504e2120284686c542c8efa5ba103f92afca
rodopiip <maria.st.radeva@gmail.com>
2025-06-22 01:13:56 +0300

    start implementing cleanButton backend and frontend logic

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
+from pydantic import BaseModel
 
+class APITransferableText(BaseModel):
+    text: str
+
+# "{"text": "alabala"}"
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/",response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "temp1": "alabala"})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 @app.post("/clean")
-async def clean_text(inbound_text: Annotated[str, Body()]):
-    return {"clean_text": inbound_text}
+async def clean_text(inbound_text: Annotated[APITransferableText, Body()]):
+    return {"clean_text": inbound_text.text}
